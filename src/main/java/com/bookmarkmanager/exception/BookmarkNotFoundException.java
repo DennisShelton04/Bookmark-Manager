714ee877731119df18eb7fd1646ff3845ea0a483
DennisShelton04 <94118933+DennisShelton04@users.noreply.github.com>
2024-09-25 17:47:37 +0530

    Revert "BookmarkManager/feature/add-BookMark-POST-GET"
    This reverts commit c3edb576cbe8899e31c42619573ccc9f1d8fc377.

--- src/main/java/com/bookmarkmanager/exception/BookmarkNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookmarkmanager.exception;
-
-public class BookmarkNotFoundException extends RuntimeException{
-  public BookmarkNotFoundException(String message) {
-    super(message);
-  }
-}
